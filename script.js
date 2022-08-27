34a05f95e3c9973836085b862ce177573d2c1911
donchi-donald <donchidonald2000@gmail.com>
2022-08-27 10:47:01 +0200

    random function add

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function getRandomLower(){
+   
+    return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+}
+
+
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+}
+
+function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+}
+
+
+function getRandomSymbol(){
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    const i = Math.floor(Math.random()*symbols.length);
+    return symbols[i];
+}
+
+
